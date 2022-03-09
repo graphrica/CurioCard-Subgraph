911f181758e3860943e2a6854165da396528850b
james-strom <93652506+Alex-the-Goat@users.noreply.github.com>
2022-03-09 14:27:11 +0200

    added callHandlers
    Added call handlers for Official wrap, unwrap, wrap batch, unwrap batch. As well as unofficial wrap and unwrap.

---
+++ src/erc1155-official-mapping.ts
@@ -0,0 +1,17 @@
+import { UnwrapCall, WrapCall } from "../generated/ERC1155/ERC1155";
+
+export function handleOfficialWrap(call: UnwrapCall): void { 
+    // LEAVE EMPTY
+   }
+
+export function handleOfficialUnwrap(call: WrapCall): void { 
+    // LEAVE EMPTY
+   }
+
+export function handleWrapBatch(call: WrapCall): void { 
+    // LEAVE EMPTY
+   }
+
+export function handleUnwrapBatch(call: UnwrapCall): void { 
+    // LEAVE EMPTY
+   }
---
+++ src/erc1155-unofficial-mapping.ts
@@ -0,0 +1,9 @@
+import { UnwrapCall, WrapCall } from "../generated/ERC1155/ERC1155";
+
+export function handleUnofficialUnwrap(call: UnwrapCall): void { 
+    // LEAVE EMPTY
+   }
+
+   export function handleOfficialUnwrap(call: WrapCall): void { 
+    // LEAVE EMPTY
+   }

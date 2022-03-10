6cb028c220fc9409d1dd3abdcde2874ea93b7e91
KENT <86975423+PtomelySoter@users.noreply.github.com>
2022-03-10 19:35:11 +0200

    initial changes

---
+++ src/erc1155Official-mapping.ts
@@ -0,0 +1,10 @@
+import { WrapCall, UnwrapCall, WrapBatchCall, UnwrapBatchCall } from "../generated/ERC1155/ERC1155";
+
+export function handleOfficialWrap(call: WrapCall): void {
+}
+export function handleOfficialUnwrap(call: UnwrapCall): void {
+}
+export function handleOfficialWrapBatch(call: WrapBatchCall): void {
+}
+export function handleOfficialUnwrapBatch(call: UnwrapBatchCall): void {
+}
---
+++ src/erc1155Unofficial-mapping.ts
@@ -0,0 +1,6 @@
+import { WrapCall, UnwrapCall } from "../generated/ERC1155Unofficial/ERC1155Unofficial";
+
+export function handleOfficialWrap(call: WrapCall): void {
+}
+export function handleOfficialUnwrap(call: UnwrapCall): void {
+}

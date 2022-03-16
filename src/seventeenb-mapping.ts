11c8b58f608ae3e40f0529597e66e2f8c9f3b6e7
KENT <86975423+PtomelySoter@users.noreply.github.com>
2022-03-16 13:56:14 +0200

    set correct supply

--- src/seventeenb-mapping.ts
+++ src/seventeenb-mapping.ts
@@ -29,7 +29,7 @@ export function handleTransfer(event: Transfer): void {
         Address.fromString("0xe0b5e6f32d657e0e18d4b3e801ebc76a5959e123").toHex()
       );
 
-      cardType.supply = BigInt.fromI32(323);
+      cardType.supply = BigInt.fromI32(500);
       cardType.address = Address.fromString(
         "0xe0b5e6f32d657e0e18d4b3e801ebc76a5959e123"
       );
